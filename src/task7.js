fc36bc7a8dd94f50bb21802cbfa41c3299050047
Leticia Janet BR <bermudez.leticia.2609@gmail.com>
2025-09-03 16:53:45 -0600

    update se hizo pruebas con una matriz (agregar,eliminar,voltear)

--- src/task7.js
+++ src/task7.js
@@ -5,7 +5,18 @@ const arr = [
   ]
   
   // Type your code below this line!
+  arr[2].push(30)
+  arr[0].splice(5,0,25);
+  //console.log(arr);
+
+  arr.push([9,8,7,6,5,4,3,2,1])
+  //console.log(arr);
+
+  arr[0].pop();
+  //console.log(arr);
+
+  arr[1].reverse();
+  //console.log(arr);
   
-  
-  
+
   // Type your code above this line!
